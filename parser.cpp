5bc62551919c7c37dfc095868cd3c19583a7fcd6
Siyue Zhou <sz132@duke.edu>
2018-02-28 21:30:29 -0500

    no change

--- parser.cpp
+++ parser.cpp
@@ -135,7 +135,7 @@ bool responseHead::parseResponse(std::vector<char> & buffer){
 
   return true;
 }
-/*
+
 int main(){
   char msg1[]= "GET /test HTTP/1.1\r\nHost: 192.241.213.46:6880\r\nUpgrade-Insecure-Requests: 1\r\n\r\n";
   char msg2[]= "HTTP/1.x 200 OK\r\nTransfer-Encoding: chunked\r\nDate: Sat, 28 Nov 2009 04:36:25 GMT\r\n\r\n";
@@ -144,9 +144,8 @@ int main(){
   responseHead head2;
   head2.parseResponse(msg2);
   return EXIT_SUCCESS;
-
 }
-*/
+
 
 
 
